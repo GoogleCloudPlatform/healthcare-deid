b7951183d6bf014fe433cfa6097fe18c04b2739a
Cloud Healthcare Team <noreply@google.com>
2019-05-31 10:07:43 -0400

    Add support for redacting DICOM based on custom keeplist or removelist.
    PiperOrigin-RevId: 250879700

--- offline_tools/redactor/src/main/java/com/google/cloud/healthcare/deid/redactor/DicomRedactor.java
+++ offline_tools/redactor/src/main/java/com/google/cloud/healthcare/deid/redactor/DicomRedactor.java
@@ -16,18 +16,144 @@
 
 package com.google.cloud.healthcare.deid.redactor;
 
+import com.google.cloud.healthcare.deid.redactor.protos.DicomConfigProtos.DicomConfig;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashSet;
+import java.util.Set;
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.Attributes.Visitor;
+import org.dcm4che3.data.StandardElementDictionary;
+import org.dcm4che3.data.Tag;
+import org.dcm4che3.data.UID;
+import org.dcm4che3.data.VR;
+import org.dcm4che3.imageio.codec.TransferSyntaxType;
+import org.dcm4che3.io.BulkDataDescriptor;
+import org.dcm4che3.io.DicomInputStream;
+import org.dcm4che3.io.DicomInputStream.IncludeBulkData;
+import org.dcm4che3.io.DicomOutputStream;
 
 /** DicomRedactor implements basic DICOM redaction. */
 public class DicomRedactor {
 
-  public DicomRedactor() throws Exception {
-    throw new Exception("Unimplemented");
+  /** RedactorSettings holds the settings for DICOM redaction. */
+  private final class RedactorSettings {
+    public Set<Integer> tagSet;
+    public boolean isKeepList;
   }
 
-  /** Redact the given DICOM input stream, and write the result to the given output stream. */
-  public void redact(InputStream inStream, OutputStream outStream) throws Exception {
-    throw new Exception("Unimplemented");
+  private final RedactorSettings settings;
+
+   /**
+   *  Iterates over all tags in a DICOM file and redacts based on tagSet. If isKeepList is true, the
+   *  tags in the tagSet are kept untouched and all others are removed. If isKeepList is false, the
+   *  tags in the tagSet are removed and all others are kept untouched.
+   */
+  private class RedactVisitor implements Visitor {
+    @Override
+    public boolean visit(Attributes attrs, int tag, VR vr, Object value) {
+      if ((settings.isKeepList && !settings.tagSet.contains(tag))
+              || (!settings.isKeepList && settings.tagSet.contains(tag))) {
+        attrs.setNull(tag, vr);
+      }
+      return true;
+    }
+  }
+
+  /**
+   * Constructs a DicomRedactor for the provided config.
+   * @throws IllegalArgumentException if the configuration structure is invalid.
+   */
+  public DicomRedactor(DicomConfig config) throws Exception {
+    this.settings = parseConfig(config);
+  }
+
+  /**
+   * Parses DicomConfig proto to produce a RedactorSettings object.
+   * @throws IllegalArgumentException if the configuration structure or tags are invalid.
+   */
+  private RedactorSettings parseConfig(DicomConfig config) throws IllegalArgumentException {
+    RedactorSettings ret = new RedactorSettings();
+    DicomConfig.TagFilterList tags;
+    switch(config.getTagFilterCase()) {
+      case KEEP_LIST:
+        ret.isKeepList = true;
+        tags = config.getKeepList();
+        break;
+      case REMOVE_LIST:
+        ret.isKeepList = false;
+        tags = config.getRemoveList();
+        break;
+      default:
+        throw new IllegalArgumentException("Config does not specify a tag filtration method.");
+    }
+    ret.tagSet = new HashSet<Integer>();
+    for (String tag : tags.getTagsList()) {
+      int tagID = this.toTagID(tag);
+      ret.tagSet.add(tagID);
+    }
+    return ret;
+  }
+
+  /**
+   * Converts tag keywords and id strings to tag IDs.
+   * @throws IllegalArgumentException if the tag cannot be converted.
+   */
+  private int toTagID(String tag) throws IllegalArgumentException {
+    // Attempt to parse as a tag keyword.
+    int tagNum = StandardElementDictionary.tagForKeyword(tag, null /* privateCreatorID */);
+    if (tagNum != -1) {
+      return tagNum;
+    }
+    // Attempt to parse as a tag id in string form.
+    int ret;
+    try {
+      ret = Integer.parseInt(tag, 16);
+    } catch (Exception e) {
+      throw new IllegalArgumentException(String.format("Failed to recognize DICOM tag: %s", tag));
+    }
+    return ret;
+  }
+
+  /**
+   * Redact the given DICOM input stream, and write the result to the given output stream.
+   * @throws IOException if the input stream cannot be read or the output stream cannot be written.
+   * @throws IllegalArgumentException if there is an error redacting the object.
+   */
+  public void redact(InputStream inStream, OutputStream outStream)
+      throws IOException, IllegalArgumentException {
+    Attributes metadata, dataset;
+    try (DicomInputStream dicomInputStream = new DicomInputStream(inStream)) {
+      dicomInputStream.setIncludeBulkData(IncludeBulkData.YES);
+      dicomInputStream.setBulkDataDescriptor(BulkDataDescriptor.PIXELDATA);
+
+      metadata = dicomInputStream.getFileMetaInformation();
+      dataset = dicomInputStream.readDataset(-1 /* len */, -1 /* stop tag */);
+    } catch (Exception e) {
+      throw new IOException("Failed to read input DICOM object", e);
+    }
+
+    try {
+      RedactVisitor visitor = new RedactVisitor();
+      dataset.accept(visitor, false /* visitNestedDatasets */);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Failed to redact one or more tags", e);
+    }
+
+    // Overwrite transfer syntax if PixelData has been removed.
+    String ts = metadata.getString(Tag.TransferSyntaxUID);
+    if (dataset.contains(toTagID("PixelData"))
+        && (!dataset.containsValue(toTagID("PixelData")))
+        && (TransferSyntaxType.forUID(ts) != TransferSyntaxType.NATIVE)) {
+      metadata.setString(Tag.TransferSyntaxUID, VR.UI, UID.ExplicitVRLittleEndian);
+    }
+
+    try (DicomOutputStream dicomOutputStream =
+        new DicomOutputStream(outStream, UID.ExplicitVRLittleEndian)) {
+      dicomOutputStream.writeDataset(metadata, dataset);
+    } catch (Exception e) {
+      throw new IOException("Failed to write output DICOM object", e);
+    }
   }
 }
--- offline_tools/redactor/src/test/java/com/google/cloud/healthcare/deid/redactor/DicomRedactorTest.java
+++ offline_tools/redactor/src/test/java/com/google/cloud/healthcare/deid/redactor/DicomRedactorTest.java
@@ -16,17 +16,353 @@
 
 package com.google.cloud.healthcare.deid.redactor;
 
+import com.google.cloud.healthcare.deid.redactor.protos.DicomConfigProtos.DicomConfig;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.Fragments;
+import org.dcm4che3.data.Implementation;
+import org.dcm4che3.data.Sequence;
+import org.dcm4che3.data.Tag;
+import org.dcm4che3.data.UID;
+import org.dcm4che3.data.VR;
+import org.dcm4che3.data.Value;
+import org.dcm4che3.io.DicomOutputStream;
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /** Test basic DICOM tag redaction. */
 @RunWith(JUnit4.class)
 public final class DicomRedactorTest {
-  @Test(expected = Exception.class)
-  public void construct_throwsException() throws Exception {
-    DicomRedactor redactor = new DicomRedactor();
+  @Rule
+  public TemporaryFolder folder = new TemporaryFolder();
 
+  private void redactAndVerify(Attributes metadata, Attributes inData, Attributes expectedMetadata,
+      Attributes expectedData, DicomConfig config) throws Exception {
+    File inFile = folder.newFile("in.dcm");
+    File outFile = folder.newFile("out.dcm");
+    File expectedFile = folder.newFile("exp.dcm");
+
+    writeDataset(inFile, metadata, inData);
+    writeDataset(expectedFile, expectedMetadata, expectedData);
+
+    InputStream is = new BufferedInputStream(new FileInputStream(inFile));
+    OutputStream os = new BufferedOutputStream(new FileOutputStream(outFile));
+
+    DicomRedactor redactor = new DicomRedactor(config);
+    redactor.redact(is, os);
+    Assert.assertTrue(FileUtils.contentEquals(outFile, expectedFile));
+  }
+
+  private Attributes getTestMetadataHeader(String transferSyntaxUID) {
+    Attributes metadata = new Attributes();
+    metadata.setBytes(Tag.FileMetaInformationVersion, VR.OB, new byte[]{0x00, 0x01});
+    metadata.setString(Tag.MediaStorageSOPInstanceUID, VR.UI, "Test");
+    metadata.setString(Tag.MediaStorageSOPClassUID, VR.UI, "Test");
+    metadata.setString(Tag.TransferSyntaxUID, VR.UI, transferSyntaxUID);
+    metadata.setString(Tag.ImplementationClassUID, VR.UI, Implementation.getClassUID());
+    metadata.setString(Tag.ImplementationVersionName, VR.SH, Implementation.getVersionName());
+    return metadata;
+  }
+
+  private void writeDataset(File f, Attributes md, Attributes dataset) throws Exception {
+    try (DicomOutputStream os = new DicomOutputStream(
+        new BufferedOutputStream(new FileOutputStream(f)), UID.ExplicitVRLittleEndian)) {
+      os.writeDataset(md, dataset);
+    }
+  }
+
+  @Test
+  public void redactRemoveList() throws Exception {
+    Attributes metadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes dataset = new Attributes();
+    dataset.setString(Tag.IdentifyingComments, VR.LT, "Test String");
+    dataset.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Attributes seq = new Attributes();
+    seq.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Sequence items = dataset.newSequence(Tag.FailedSOPSequence, 1);
+    items.add(seq);
+
+    List<String> tags = new ArrayList<String>();
+    tags.add("PatientName");
+    DicomConfig config = DicomConfig.newBuilder().setRemoveList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    Attributes expected = new Attributes();
+    expected.setString(Tag.IdentifyingComments, VR.LT, "Test String");
+    expected.setString(Tag.PatientName, VR.PN, "");
+    Attributes expSeq = new Attributes();
+    expSeq.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Sequence expectedItems = expected.newSequence(Tag.FailedSOPSequence, 1);
+    expectedItems.add(expSeq);
+
+    redactAndVerify(metadata, dataset, metadata, expected, config);
+  }
+
+  @Test
+  public void redactRemoveListNumberedTag() throws Exception {
+    Attributes metadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes dataset = new Attributes();
+    dataset.setString(Tag.IdentifyingComments, VR.LT, "Test String");
+    dataset.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Attributes seq = new Attributes();
+    seq.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Sequence singleItemSequence = dataset.newSequence(Tag.FailedSOPSequence, 1);
+    singleItemSequence.add(seq);
+
+    List<String> tags = new ArrayList<String>();
+    tags.add("00100010"); // PatientName
+    DicomConfig config = DicomConfig.newBuilder().setRemoveList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    Attributes expected = new Attributes();
+    expected.setString(Tag.IdentifyingComments, VR.LT, "Test String");
+    expected.setString(Tag.PatientName, VR.PN, "");
+    Attributes expSeq = new Attributes();
+    expSeq.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Sequence expectedItems = expected.newSequence(Tag.FailedSOPSequence, 1);
+    expectedItems.add(expSeq);
+
+    redactAndVerify(metadata, dataset, metadata, expected, config);
+  }
+
+  @Test
+  public void redactRemoveListSequence() throws Exception {
+    Attributes metadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes dataset = new Attributes();
+    Attributes seq = new Attributes();
+    seq.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Sequence singleItemSequence = dataset.newSequence(Tag.FailedSOPSequence, 1);
+    singleItemSequence.add(seq);
+
+    List<String> tags = new ArrayList<String>();
+    tags.add("00081198"); // FailedSOPSequence
+    DicomConfig config = DicomConfig.newBuilder().setRemoveList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    Attributes expected = new Attributes();
+    Sequence expectedItems = expected.newSequence(Tag.FailedSOPSequence, 0);
+
+    redactAndVerify(metadata, dataset, metadata, expected, config);
+  }
+
+  @Test
+  public void redactRemoveListEmpty() throws Exception {
+     Attributes metadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes dataset = new Attributes();
+    dataset.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Attributes seq = new Attributes();
+    seq.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Sequence singleItemSequence = dataset.newSequence(Tag.FailedSOPSequence, 1);
+    singleItemSequence.add(seq);
+
+    List<String> tags = new ArrayList<String>();
+    DicomConfig config = DicomConfig.newBuilder().setRemoveList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    redactAndVerify(metadata, dataset, metadata, dataset, config);
+  }
+
+  @Test
+  public void uncompressedPixelData() throws Exception {
+    Attributes metadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes dataset = new Attributes();
+    dataset.setBytes(Tag.PixelData, VR.OB,
+        new byte[]{(byte) 0xAB, (byte) 0xCD, (byte) 0xEF, (byte) 0x01});
+
+    List<String> tags = new ArrayList<String>();
+    tags.add("PixelData");
+    DicomConfig config = DicomConfig.newBuilder().setRemoveList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    Attributes expectedMetadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes expected = new Attributes();
+    expected.setBytes(Tag.PixelData, VR.OB, new byte[]{});
+
+    redactAndVerify(metadata, dataset, expectedMetadata, expected, config);
+  }
+
+  @Test
+  public void singleFragmentPixelData() throws Exception {
+    Attributes metadata = getTestMetadataHeader(UID.JPEGBaseline1);
+    Attributes dataset = new Attributes();
+    Fragments fragments = dataset.newFragments(Tag.PixelData, VR.OB, 2);
+    fragments.add(Value.NULL);
+    fragments.add(new byte[]{(byte) 0xAB, (byte) 0xCD});
+
+    List<String> tags = new ArrayList<String>();
+    tags.add("PixelData");
+    DicomConfig config = DicomConfig.newBuilder().setRemoveList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    Attributes expectedMetadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes expected = new Attributes();
+    expected.setBytes(Tag.PixelData, VR.OB, new byte[]{});
+
+    redactAndVerify(metadata, dataset, expectedMetadata, expected, config);
+  }
+
+  @Test
+  public void bigEndianPixelData() throws Exception {
+    Attributes metadata = getTestMetadataHeader(UID.ExplicitVRBigEndianRetired);
+    Attributes dataset = new Attributes();
+    dataset.setBytes(Tag.PixelData, VR.OB,
+        new byte[]{(byte) 0xAB, (byte) 0xCD, (byte) 0xEF, (byte) 0x01});
+
+    List<String> tags = new ArrayList<String>();
+    tags.add("PixelData");
+    DicomConfig config = DicomConfig.newBuilder().setRemoveList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    Attributes expected = new Attributes();
+    expected.setBytes(Tag.PixelData, VR.OB, new byte[]{});
+
+    redactAndVerify(metadata, dataset, metadata, expected, config);
+  }
+
+  @Test
+  public void redactKeepList() throws Exception {
+    Attributes metadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes dataset = new Attributes();
+    dataset.setString(Tag.IdentifyingComments, VR.LT, "Test String");
+    dataset.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Attributes seq = new Attributes();
+    seq.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Sequence singleItemSequence = dataset.newSequence(Tag.FailedSOPSequence, 1);
+    singleItemSequence.add(seq);
+
+    List<String> tags = new ArrayList<String>();
+    tags.add("PatientName");
+    DicomConfig config = DicomConfig.newBuilder().setKeepList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    Attributes expected = new Attributes();
+    expected.setString(Tag.IdentifyingComments, VR.LT, "");
+    expected.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Sequence expectedItems = expected.newSequence(Tag.FailedSOPSequence, 0);
+
+    redactAndVerify(metadata, dataset, metadata, expected, config);
+  }
+
+  @Test
+  public void redactKeepListEmpty() throws Exception {
+    Attributes metadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes dataset = new Attributes();
+    dataset.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Attributes seq = new Attributes();
+    seq.setString(Tag.PatientName, VR.PN, "Person^Name");
+    Sequence singleItemSequence = dataset.newSequence(Tag.FailedSOPSequence, 1);
+    singleItemSequence.add(seq);
+
+    List<String> tags = new ArrayList<String>();
+    DicomConfig config = DicomConfig.newBuilder().setKeepList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    Attributes expected = new Attributes();
+    expected.setString(Tag.PatientName, VR.PN, "");
+    Sequence expectedItems = expected.newSequence(Tag.FailedSOPSequence, 0);
+
+    redactAndVerify(metadata, dataset, metadata, expected, config);
+  }
+
+  @Test
+  public void littleEndianImplicitTransferSyntax() throws Exception {
+    Attributes metadata = getTestMetadataHeader(UID.ImplicitVRLittleEndian);
+    Attributes dataset = new Attributes();
+    dataset.setString(Tag.PatientName, VR.PN, "Person^Name");
+
+    List<String> tags = new ArrayList<String>();
+    tags.add("PatientName");
+    DicomConfig config = DicomConfig.newBuilder().setRemoveList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    Attributes expected = new Attributes();
+    expected.setString(Tag.PatientName, VR.PN, "");
+
+    redactAndVerify(metadata, dataset, metadata, expected, config);
+  }
+
+
+  @Test(expected = IllegalArgumentException.class)
+  public void invalidTag() throws Exception {
+     Attributes metadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes dataset = new Attributes();
+    dataset.setString(Tag.PatientName, VR.PN, "Person^Name");
+
+    List<String> tags = new ArrayList<String>();
+    tags.add("invalidTag123");
+    DicomConfig config = DicomConfig.newBuilder().setRemoveList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    redactAndVerify(metadata, dataset, metadata, dataset, config);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void emptyConfig() throws Exception {
+     Attributes metadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes dataset = new Attributes();
+    dataset.setString(Tag.PatientName, VR.PN, "Person^Name");
+
+    DicomConfig config = DicomConfig.newBuilder().build();
+
+    redactAndVerify(metadata, dataset, metadata, dataset, config);
+  }
+
+  @Test(expected = IOException.class)
+  public void failRead() throws Exception {
+     Attributes metadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes dataset = new Attributes();
+    dataset.setString(Tag.PatientName, VR.PN, "Person^Name");
+
+    List<String> tags = new ArrayList<String>();
+    DicomConfig config = DicomConfig.newBuilder().setRemoveList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    File inFile = folder.newFile("in.dcm");
+    writeDataset(inFile, metadata, dataset);
+    File outFile = folder.newFile("out.dcm");
+
+    InputStream is = new BufferedInputStream(new FileInputStream(inFile));
+    is.close();
+    OutputStream os = new BufferedOutputStream(new FileOutputStream(outFile));
+
+    DicomRedactor redactor = new DicomRedactor(config);
+    redactor.redact(is, os);
+  }
+
+  @Test(expected = IOException.class)
+  public void failWrite() throws Exception {
+     Attributes metadata = getTestMetadataHeader(UID.ExplicitVRLittleEndian);
+    Attributes dataset = new Attributes();
+    dataset.setString(Tag.PatientName, VR.PN, "Person^Name");
+
+    List<String> tags = new ArrayList<String>();
+    DicomConfig config = DicomConfig.newBuilder().setRemoveList(
+        DicomConfig.TagFilterList.newBuilder().addAllTags(tags)).build();
+
+    File inFile = folder.newFile("in.dcm");
+    writeDataset(inFile, metadata, dataset);
+    File outFile = folder.newFile("out.dcm");
+
+    InputStream is = new BufferedInputStream(new FileInputStream(inFile));
+    OutputStream os = new BufferedOutputStream(new FileOutputStream(outFile));
+    os.close();
+
+    DicomRedactor redactor = new DicomRedactor(config);
+    redactor.redact(is, os);
   }
 }
 
