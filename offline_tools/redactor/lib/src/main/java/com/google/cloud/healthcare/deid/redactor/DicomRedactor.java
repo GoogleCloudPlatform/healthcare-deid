8bf0e29a437c7212332f4d2165c82cfed1fcdb46
Egor Radchenko <egor.radchenko@quantumsoft.ru>
2019-10-11 14:20:16 +0700

    edit a stream without fully reading it

--- offline_tools/redactor/lib/src/main/java/com/google/cloud/healthcare/deid/redactor/DicomRedactor.java
+++ offline_tools/redactor/lib/src/main/java/com/google/cloud/healthcare/deid/redactor/DicomRedactor.java
@@ -19,6 +19,7 @@ package com.google.cloud.healthcare.deid.redactor;
 import com.google.cloud.healthcare.deid.redactor.protos.DicomConfigProtos.DicomConfig;
 import com.google.protobuf.TextFormat;
 import java.io.BufferedReader;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -44,6 +45,8 @@ import org.dcm4che3.util.UIDUtils;
 /** DicomRedactor implements basic DICOM redaction. */
 public class DicomRedactor {
 
+  private static final int BATCH_SIZE = 8192;
+
   /** RedactorSettings holds the settings for DICOM redaction. */
   private final class RedactorSettings {
     public Set<Integer> tagSet;
@@ -68,8 +71,7 @@ public class DicomRedactor {
         DicomRedactor.this.regenUID(attrs, tag);
         return true;
       }
-      if ((settings.isKeepList && !settings.tagSet.contains(tag))
-              || (!settings.isKeepList && settings.tagSet.contains(tag))) {
+      if (shouldRemoveTag(tag)) {
         attrs.setNull(tag, vr);
       }
       return true;
@@ -175,12 +177,18 @@ public class DicomRedactor {
     attrs.setString(tag, VR.UI, newUID);
   }
 
+  private boolean shouldRemoveTag(int tag) {
+    return (settings.isKeepList && !settings.tagSet.contains(tag))
+        || (!settings.isKeepList && settings.tagSet.contains(tag));
+  }
+
   /**
    * Redact the given DICOM input stream, and write the result to the given output stream.
    * @throws IOException if the input stream cannot be read or the output stream cannot be written.
    * @throws IllegalArgumentException if there is an error redacting the object.
    */
-  public void redact(InputStream inStream, OutputStream outStream)
+  @Deprecated
+  public void redactWithFullRead(InputStream inStream, OutputStream outStream)
       throws IOException, IllegalArgumentException {
     Attributes metadata, dataset;
     try (DicomInputStream dicomInputStream = new DicomInputStream(inStream)) {
@@ -216,6 +224,67 @@ public class DicomRedactor {
       dicomOutputStream.writeDataset(metadata, dataset);
     } catch (Exception e) {
       throw new IOException("Failed to write output DICOM object", e);
+    }
+  }
+
+  /**
+   * Redact the given DICOM input stream, and write the result to the given output stream. Without
+   * reading whole dataset in one go.
+   *
+   * @throws IOException if the input stream cannot be read or the output stream cannot be written.
+   * @throws IllegalArgumentException if there is an error redacting the object.
+   */
+  public void redact(InputStream inStream, OutputStream outStream)
+      throws IOException, IllegalArgumentException {
+    Attributes metadata, dataset;
+    RedactVisitor visitor = new RedactVisitor();
+    try (DicomInputStream dicomInputStream = new DicomInputStream(inStream)) {
+      try (DicomOutputStream dicomOutputStream = new DicomOutputStream(outStream,
+          UID.ExplicitVRLittleEndian)) {
+        try {
+          dicomInputStream.setIncludeBulkData(IncludeBulkData.YES);
+          dicomInputStream.setBulkDataDescriptor(BulkDataDescriptor.PIXELDATA);
+          metadata = dicomInputStream.getFileMetaInformation();
+
+          // Update UID in metadata.
+          regenUID(metadata, Tag.MediaStorageSOPInstanceUID);
+
+          // Overwrite transfer syntax if PixelData is to be removed.
+          String ts = metadata.getString(Tag.TransferSyntaxUID);
+          if (shouldRemoveTag(toTagID("PixelData"))
+              && (TransferSyntaxType.forUID(ts) != TransferSyntaxType.NATIVE)) {
+            metadata.setString(Tag.TransferSyntaxUID, VR.UI, UID.ExplicitVRLittleEndian);
+          }
+        } catch (Exception e) {
+          throw new IOException("Failed to initialize redactor", e);
+        }
+
+        boolean firstDataset = true;
+        boolean lastDataset = false;
+        while (!lastDataset) {
+          try {
+            dicomInputStream.mark(BATCH_SIZE);
+            dataset = dicomInputStream.readDataset(BATCH_SIZE, -1);
+          } catch (EOFException e) {
+            dicomInputStream.reset();
+            dataset = dicomInputStream.readDataset(-1, -1);
+            lastDataset = true;
+          }
+
+          try {
+            dataset.accept(visitor, false);
+          } catch (Exception e) {
+            throw new IllegalArgumentException("Failed to redact one or more tags", e);
+          }
+
+          if (firstDataset) {
+            firstDataset = false;
+            dicomOutputStream.writeDataset(metadata, dataset);
+          } else {
+            dicomOutputStream.writeDataset(null, dataset);
+          }
+        }
+      }
     }
   }
 }
